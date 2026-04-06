5b43a5761f5edeae077dac541f82269788a4dcab
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2026-04-06 09:11:58 -0400

    Update MediaImport.py

--- MediaImport.py
+++ MediaImport.py
@@ -40,7 +40,7 @@ def removevff(plex, mediaid, message):
     for stream in audio_streams:
         logging.info(f"Audio Title: {stream.title} - Language: {stream.languageCode}")
         if stream.languageCode == f"fra" and stream.title:
-            if (any(sub in f"{stream.title.lower()}" for sub in vfqstrings.lower())) and stream.streamType() = 2:
+            if (any(sub in f"{stream.title.lower()}" for sub in vfqstrings.lower())) and stream.streamType() == 2:
                 vfq.append(f"{stream.index}")
             else:
                 notvfq.append(f"{stream.index}")
