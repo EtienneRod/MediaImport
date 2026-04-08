51db17f16cee3c3fa6834f9a44552ef12c6f97a5
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2026-04-08 11:57:57 -0400

    Update MediaImport.py

--- MediaImport.py
+++ MediaImport.py
@@ -96,7 +96,7 @@ def labeling(plex, message):
 def plex_webhook():
     pushovermsg=f""
     data = json.loads(request.form['payload'])
-    logging.info(f"{data}")
+    #logging.info(f"{data}")
     logging.info(f"{data["Metadata"]["librarySectionTitle"]}")
     if data["event"] == f"library.new" and (data["Metadata"]["librarySectionTitle"] == f"Films" or data["Metadata"]["librarySectionTitle"] == f"Série TV" ):
         logging.info(f"{data}")
