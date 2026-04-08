fbecc6e27a395f8648a90ddd85c758d04f67699f
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2026-04-08 09:06:21 -0400

    Update MediaImport.py

--- MediaImport.py
+++ MediaImport.py
@@ -20,7 +20,7 @@ flaskPort=f"{os.environ.get('FLASK_PORT')}"
 contentrating=f"{os.environ.get('CONTENT_RATING')}".split(',')
 commonsenseage=int(f"{os.environ.get('COMMONSENSE_AGE')}")
 audiolanguage=f"{os.environ.get('AUDIO_LANGUAGE')}".split(',')
-excludedlabels=f"{os.environ.get('EXCLUDED_LABLES')}".split(',')
+excludedlabels=f"{os.environ.get('EXCLUDED_LABELS')}".split(',')
 vfqstrings=f"{os.environ.get('VFQ_STRINGS')}".lower().split(',')
 
 # Define Flask
