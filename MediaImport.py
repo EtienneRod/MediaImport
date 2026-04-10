f9eee34c7a253757982b6e9adde2877a89691fac
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2026-04-10 09:16:17 -0400

    Update MediaImport.py

--- MediaImport.py
+++ MediaImport.py
@@ -66,11 +66,11 @@ def labeling(plex, message):
     logging.info(f"------------------------------------------------------------")
     logging.info(f"Starting Labeling")
     medias = plex.library.section(f"Movies").search(filters = {f"label!":f"{excludedlabels}",
-                                                                      f"contentRating|":f"{contentrating}",
-                                                                      f"audioLanguage|":f"{audiolanguage}"})
+                                                               f"contentRating|":f"{contentrating}",
+                                                               f"audioLanguage|":f"{audiolanguage}"})
     medias = medias + plex.library.section(f"Films").search(filters = {f"label!":f"{excludedlabels}",
-                                                                          f"contentRating|":f"{contentrating}",
-                                                                          f"audioLanguage|":f"{audiolanguage}"})
+                                                                       f"contentRating|":f"{contentrating}",
+                                                                       f"audioLanguage|":f"{audiolanguage}"})
     medias = medias + plex.library.section(f"TV Shows").search(filters = {f"label!":f"{excludedlabels}",
                                                                           f"contentRating|":f"{contentrating}",
                                                                           f"audioLanguage|":f"{audiolanguage}"})
